e128a8b80b5df397ef1339e70e9d67bcfad987e7
tammiekoh1518 <e0797744@u.nus.edu>
2023-12-13 19:28:25 +0800

    tried fixing index.html file

--- pdfapp/urls.py
+++ pdfapp/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
-from .views import index
 
 urlpatterns = [
     path('upload/', views.file_upload, name='file_upload'),
-    path('', index, name='index'),
+    path('', views.home, name='home'), 
 ]
